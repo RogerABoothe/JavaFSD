2e3aa1a32f6934c839bb39c59bf852ef56370152
RogerABoothe <Roger.A.Boothe@Gmail.com>
2022-05-17 18:16:47 -0500

    Initial commit

---
+++ 10questions/com.java
@@ -0,0 +1,187 @@
+import java.util.Scanner;
+
+public class com {
+
+	public static void main(String[] args) {
+		int userSelection ;
+		int userScore = 0;
+		Scanner scr = new Scanner(System.in);
+		
+		System.out.println("Welcome to the Java FSD quiz! You will be tested on various questions about Java.");
+		System.out.println("Before we being, please note that this is a graded quiz.");
+		System.out.println("To pass you need to get seven (7) or more points!");
+		System.out.println("Good luck!");
+		
+		// question 1 
+		System.out.println("Question 1");
+		System.out.println("What are the two types of data types");
+		System.out.println("1 - Primitive and non primitive");
+		System.out.println("2 - Primaative and Advanced");
+		System.out.println("3 - Modern and Future");
+		System.out.println("4 - Int and String");
+		userSelection = scr.nextInt();
+		
+		if ( userSelection == 1 ) {
+			userScore = userScore + 1;
+			System.out.println("That is correct!");
+		}
+		else {
+			System.out.println("Your answer was not correct");
+		}
+		
+		// question 2
+		System.out.println("Question 2");
+		System.out.println("Wha is the byte count for an int");
+		System.out.println("1 - 2 bytes");
+		System.out.println("2 - 4 bytes");
+		System.out.println("3 - 8 bytes");
+		System.out.println("4 - All this talk of bytes makes me hungry");
+		userSelection = scr.nextInt();
+		
+		if ( userSelection == 2 ) {
+			userScore = userScore + 1;
+			System.out.println("That is correct!");
+		}
+		else {
+			System.out.println("Your answer was not correct");
+		}
+		
+		// question 3
+		System.out.println("Question 3");
+		System.out.println("Wha is the byte count for an int");
+		System.out.println("1 - 2 bytes");
+		System.out.println("2 - 4 bytes");
+		System.out.println("3 - 8 bytes");
+		System.out.println("4 - All this talk of bytes makes me hungry");
+		userSelection = scr.nextInt();
+		
+		if ( userSelection == 2 ) {
+			userScore = userScore + 1;
+			System.out.println("That is correct!");
+		}
+		else {
+			System.out.println("Your answer was not correct");
+		}
+		
+		// question 4
+		System.out.println("Question 4");
+		System.out.println("Wha is the byte count for an int");
+		System.out.println("1 - 2 bytes");
+		System.out.println("2 - 4 bytes");
+		System.out.println("3 - 8 bytes");
+		System.out.println("4 - All this talk of bytes makes me hungry");
+		userSelection = scr.nextInt();
+		
+		if ( userSelection == 2 ) {
+			userScore = userScore + 1;
+			System.out.println("That is correct!");
+		}
+		else {
+			System.out.println("Your answer was not correct");
+		}
+		
+		// question 5
+		System.out.println("Question 5");
+		System.out.println("Wha is the byte count for an int");
+		System.out.println("1 - 2 bytes");
+		System.out.println("2 - 4 bytes");
+		System.out.println("3 - 8 bytes");
+		System.out.println("4 - All this talk of bytes makes me hungry");
+		userSelection = scr.nextInt();
+		
+		if ( userSelection == 2 ) {
+			userScore = userScore + 1;
+			System.out.println("That is correct!");
+		}
+		else {
+			System.out.println("Your answer was not correct");
+		}
+	
+		// question 6
+		System.out.println("Question 6");
+		System.out.println("Wha is the byte count for an int");
+		System.out.println("1 - 2 bytes");
+		System.out.println("2 - 4 bytes");
+		System.out.println("3 - 8 bytes");
+		System.out.println("4 - All this talk of bytes makes me hungry");
+		userSelection = scr.nextInt();
+		
+		if ( userSelection == 2 ) {
+			userScore = userScore + 1;
+			System.out.println("That is correct!");
+		}
+		else {
+			System.out.println("Your answer was not correct");
+		}
+		
+		// question 7
+		System.out.println("Question 7");
+		System.out.println("Wha is the byte count for an int");
+		System.out.println("1 - 2 bytes");
+		System.out.println("2 - 4 bytes");
+		System.out.println("3 - 8 bytes");
+		System.out.println("4 - All this talk of bytes makes me hungry");
+		userSelection = scr.nextInt();
+		
+		if ( userSelection == 2 ) {
+			userScore = userScore + 1;
+			System.out.println("That is correct!");
+		}
+		else {
+			System.out.println("Your answer was not correct");
+		}
+		
+		// question 8
+		System.out.println("Question 8");
+		System.out.println("Wha is the byte count for an int");
+		System.out.println("1 - 2 bytes");
+		System.out.println("2 - 4 bytes");
+		System.out.println("3 - 8 bytes");
+		System.out.println("4 - All this talk of bytes makes me hungry");
+		userSelection = scr.nextInt();
+		
+		if ( userSelection == 2 ) {
+			userScore = userScore + 1;
+			System.out.println("That is correct!");
+		}
+		else {
+			System.out.println("Your answer was not correct");
+		}
+		
+		// question 9
+		System.out.println("Question 9");
+		System.out.println("Wha is the byte count for an int");
+		System.out.println("1 - 2 bytes");
+		System.out.println("2 - 4 bytes");
+		System.out.println("3 - 8 bytes");
+		System.out.println("4 - All this talk of bytes makes me hungry");
+		userSelection = scr.nextInt();
+		
+		if ( userSelection == 2 ) {
+			userScore = userScore + 1;
+			System.out.println("That is correct!");
+		}
+		else {
+			System.out.println("Your answer was not correct");
+		}
+		
+		// question 10
+		System.out.println("Question 10");
+		System.out.println("Wha is the byte count for an int");
+		System.out.println("1 - 2 bytes");
+		System.out.println("2 - 4 bytes");
+		System.out.println("3 - 8 bytes");
+		System.out.println("4 - All this talk of bytes makes me hungry");
+		userSelection = scr.nextInt();
+		
+		if ( userSelection == 2 ) {
+			userScore = userScore + 1;
+			System.out.println("That is correct!");
+		}
+		else {
+			System.out.println("Your answer was not correct");
+		}
+
+	}
+
+}
